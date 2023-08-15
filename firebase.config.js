ffdb256a206dc22ecf47b502acf14214798f59b0
ahmadovski <ahmad.ataei70@gmail.com>
2023-08-15 20:14:55 +0430

    initial commit. add firebase

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8sHUyZO6XY-SJuPj-KFiKC00_elwQAcg",
+  authDomain: "house-marketing-app-af6a8.firebaseapp.com",
+  projectId: "house-marketing-app-af6a8",
+  storageBucket: "house-marketing-app-af6a8.appspot.com",
+  messagingSenderId: "574331094405",
+  appId: "1:574331094405:web:ddff1c0e1f6052b228b0ec",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      my App
+    </>
+  )
+}
+
+export default App
