55174c62c9a69c6ec4e2e54d44b457c87e1af04c
ahmadovski <ahmad.ataei70@gmail.com>
2023-08-16 20:44:14 +0430

    add pages and routes ,also add react router dom as dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-
+import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
+import Explore from "./pages/Explore/Explore"
+import Profile from "./pages/Profile/Profile"
+import Offer from "./pages/Offer/Offer"
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword"
+import SignIn from "./pages/SignIn/SignIn"
+import SignUp from "./pages/SignUp/SignUp"
 
 function App() {
   
 
   return (
     <>
-      my App
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore/>} />
+        <Route path="/Profile" element={<Profile/>} />
+        <Route path="/Sign-in" element={<SignIn/>} />
+        <Route path="/Sign-up" element={<SignUp/>} />
+        <Route path="/Forgot-Password" element={<ForgotPassword/>} />
+        <Route path="/Offer" element={<Offer/>} />
+      </Routes>
+      {/* navbar */}
+    </Router>
+      
     </>
   )
 }
