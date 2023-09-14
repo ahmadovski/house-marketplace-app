8b87426b824957135e9ad1c5b9b0e877036ba2e7
ahmadovski <ahmad.ataei70@gmail.com>
2023-09-14 15:19:30 +0330

    store files & add to listings db

--- src/pages/CreateListing/CreateListing.jsx
+++ src/pages/CreateListing/CreateListing.jsx
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase.config";
+import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../../components/Spinner/Spinner";
 import { toast } from "react-toastify";
@@ -43,6 +52,49 @@ function CreateListing() {
 
   const navigate = useNavigate();
 
+  //upload to firebase storage
+  const storeImage = async (image) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage();
+
+      const userCredential = auth.currentUser;
+      const fileName = `${userCredential.uid}-${image.name}-${uuidv4()}`;
+
+      const storageRef = ref(storage, `images/` + fileName);
+
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          //progress
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  //submit
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -87,9 +139,39 @@ function CreateListing() {
       console.log(formData);
     }
 
+    //FileList to Array
+    const imagesArray = [...images];
+    //store all images and get urls
+    const imgUrls = await Promise.all(
+      imagesArray.map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("could not upload images");
+      return;
+    });
+
+    //clean up form data for storage
+    const formDataCopy = {
+      ...formData,
+      geolocation,
+      location,
+      imgUrls,
+      timestamp: serverTimestamp(),
+    };
+    delete formDataCopy.address;
+    delete formDataCopy.images;
+    location && (formDataCopy.location = location);
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    //add doc to db
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    console.log(docRef);
     setLoading(false);
+    toast.success("listing added successfully");
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   };
 
+  //like onChange
   const onMutate = (e) => {
     //boolean inputs
     let bool = null;
